31ad843b30e5f3fce332c3391b2f471322b9fa56
RistoKuy <aristo.baadi@gmail.com>
2024-10-20 11:59:03 +0700

    Update

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -5,9 +5,9 @@ class Dashboard extends CI_Controller {
     public function index()
     {
         $data['barang'] = $this->model_barang->tampil_data('')->result();
-        $this->load->view('template/header');
-        $this->load->view('template/sidebar');
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
         $this->load->view('dashboard', $data);
-        $this->load->view('template/footer');
+        $this->load->view('templates/footer');
     }
 }
