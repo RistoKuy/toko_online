28e90b1fa849bf71f650a6cd0a57f8cf7a02c4f2
RistoKuy <aristo.baadi@gmail.com>
2024-10-20 12:44:01 +0700

    Update Edit

--- application/controllers/admin/data_barang.php
+++ application/controllers/admin/data_barang.php
@@ -9,6 +9,7 @@ class Data_barang extends CI_Controller
         $this->load->view('admin/data_barang', $data);
         $this->load->view('templates_admin/footer');
     }
+
     public function tambah_aksi(){
         $nama_brg   = $this->input->post('nama_brg');
         $keterangan = $this->input->post('keterangan');
@@ -16,27 +17,78 @@ class Data_barang extends CI_Controller
         $harga      = $this->input->post('harga');
         $stok       = $this->input->post('stok');
         $gambar     = $_FILES['gambar']['name'];
-        if ($gambar=''){}else{
-            $config ['upload_path'] = './uploads';
-            $config ['allowed_types'] = 'jpg|jpeg|png|gif';
+
+        // Dapatkan id_brg terbesar dan tambahkan 1
+        $max_id = $this->model_barang->get_max_id('tb_barang');
+        $id_brg = $max_id + 1;
+
+        if ($gambar == '') {
+            echo "Gambar tidak boleh kosong";
+        } else {
+            $config['upload_path']   = './assets/uploads';
+            $config['allowed_types'] = 'jpg|jpeg|png|gif';
+            $config['file_name']     = $id_brg . '_' . $gambar; // Optional: Rename the file to include id_brg
 
             $this->load->library('upload', $config);
-            if(!$this->upload->do_upload('gambar')){
-                echo "Gagal Upload";
-            }else{
-                $gambar = $this->upload->data('file_name');
+
+            if (!$this->upload->do_upload('gambar')) {
+                echo "Gagal Upload: " . $this->upload->display_errors();
+            } else {
+                $upload_data = $this->upload->data();
+                $gambar = $upload_data['file_name'];
+            }
+        }
+
+        $data = array(
+            'id_brg'     => $id_brg,
+            'nama_brg'   => $nama_brg,
+            'keterangan' => $keterangan,
+            'kategori'   => $kategori,
+            'harga'      => $harga,
+            'stok'       => $stok,
+            'gambar'     => $gambar
+        );
+
+        $this->model_barang->tambah_barang($data, 'tb_barang');
+        redirect('admin/data_barang/index');
     }
 
-    $data = array(
-        'nama_brg'   => $nama_brg,
-        'keterangan' => $keterangan,
-        'kategori'   => $kategori,
-        'harga'      => $harga,
-        'stok'       => $stok,
-        'gambar'     => $gambar
-    );
-    $this->model_barang->tambah_barang($data,'tb_barang');
-    redirect('admin/data_barang/index');
+    public function hapus($id){
+        $where = array('id_brg' => $id);
+        $this->model_barang->hapus_data($where, 'tb_barang');
+        redirect('admin/data_barang/index');
     }
+
+    public function edit($id){
+        $where = array('id_brg' => $id);
+        $data['barang'] = $this->model_barang->edit_data($where, 'tb_barang')->result();
+        $this->load->view('templates_admin/header');
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/edit_barang', $data);
+        $this->load->view('templates_admin/footer');
+    }
+
+    public function update(){
+        $id         = $this->input->post('id_brg');
+        $nama_brg   = $this->input->post('nama_brg');
+        $keterangan = $this->input->post('keterangan');
+        $kategori   = $this->input->post('kategori');
+        $harga      = $this->input->post('harga');
+        $stok       = $this->input->post('stok');
+
+        $data = array(
+            'nama_brg'   => $nama_brg,
+            'keterangan' => $keterangan,
+            'kategori'   => $kategori,
+            'harga'      => $harga,
+            'stok'       => $stok
+        );
+
+        $where = array(
+            'id_brg' => $id
+        );
+
+        $this->model_barang->update_data($where, $data, 'tb_barang');
+        redirect('admin/data_barang/index');
     }
 }
--- application/views/admin/data_barang.php
+++ application/views/admin/data_barang.php
@@ -6,6 +6,8 @@
     <title>Data Barang</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
     <div class="container-fluid">
