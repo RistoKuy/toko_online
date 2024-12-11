9bfe461d1401f1bbd2ecef22dc5f2c2306775804
RistoKuy <aristo.baadi@gmail.com>
2024-12-12 00:18:56 +0700

    Skema DB baru

---
+++ SQL/tb_user.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `tb_user` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role_id` tinyint(1) NOT NULL
+)
+
+INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `role_id`) VALUES
+(1, 'admin', 'admin', '123', 1),
+(2, 'user', 'user', '123', 2);
